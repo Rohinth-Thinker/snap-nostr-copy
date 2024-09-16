2f4457cee539a81a81e9bfae5500c5c1456ee982
DJ Hemath <djhemath@gmail.com>
2024-09-16 11:45:56 +0530

    feat: help button component

---
+++ src/components/HelpButton/HelpButton.styled.tsx
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const StyledHelpButton = styled.button`
+    border: none;
+    background-color: transparent;
+    cursor: pointer;
+    font-size: 36px;
+    color: #FFF;
+    width: 65px;
+    border-radius: 5px;
+
+    &:hover {
+        background-color: rgba(39,39,40,1);
+    }
+`;
---
+++ src/components/HelpButton/HelpButton.tsx
@@ -0,0 +1,7 @@
+import { StyledHelpButton } from "./HelpButton.styled";
+
+export function HelpButton() {
+    return (
+        <StyledHelpButton className="rammetto-one-regular">?</StyledHelpButton>
+    );
+}
