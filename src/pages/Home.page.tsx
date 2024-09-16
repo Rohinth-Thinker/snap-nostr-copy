8052a0a017e70b7d5de4c011fb179c4843e4b8ac
DJ Hemath <djhemath@gmail.com>
2024-09-16 15:39:51 +0530

    feat: integrate toolbar

--- src/pages/Home.page.tsx
+++ src/pages/Home.page.tsx
@@ -1,10 +1,15 @@
 import { Header } from "../components/Header/Header";
-import { HomePageContainer } from "./HomePage.styled";
+import { Toolbar } from "../components/Toolbar/Toolbar";
+import { HomePageContainer, MainSection } from "./HomePage.styled";
 
 function HomePage() {
     return (
       <HomePageContainer>
         <Header/>
+
+        <MainSection>
+          <Toolbar/>
+        </MainSection>
       </HomePageContainer>
     );
   }
--- src/pages/HomePage.styled.ts
+++ src/pages/HomePage.styled.ts
@@ -5,4 +5,15 @@ export const HomePageContainer = styled.main`
     width: 100vw;
     background: rgb(24,25,26);
     background: linear-gradient(130deg, rgba(24,25,26,1) 0%, rgba(39,39,40,1) 100%);
+    display: flex;
+    flex-direction: column;
+`;
+
+export const MainSection = styled.article`
+    padding: 20px 60px;
+
+    flex: 1;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
 `;
