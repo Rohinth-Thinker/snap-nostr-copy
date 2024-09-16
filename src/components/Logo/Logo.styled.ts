624adb4a4c1fda2d0dd9a6460ee4964ca41cf801
DJ Hemath <djhemath@gmail.com>
2024-09-16 11:41:17 +0530

    feat: logo component

---
+++ src/components/Logo/Logo.styled.ts
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const StyledLogo = styled.h1`
+    color: #FFF;
+    font-size: 40px;
+    margin: 0px;
+    background: linear-gradient(90deg, #CE66FF, #FE983B);
+    background-clip: text;
+    -webkit-text-fill-color: transparent;
+    user-select: none;
+`;
---
+++ src/components/Logo/Logo.tsx
@@ -0,0 +1,5 @@
+import { StyledLogo } from "./Logo.styled";
+
+export function Logo() {
+    return <StyledLogo className="rammetto-one-regular">SnapNostr</StyledLogo>
+}
