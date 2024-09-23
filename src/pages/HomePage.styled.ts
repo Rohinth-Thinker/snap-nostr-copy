06eac131dc896230ee9b4abf2c425d8923057900
DJ Hemath <djhemath@gmail.com>
2024-09-23 22:50:33 +0530

    fix: ui fix

--- src/pages/HomePage.styled.ts
+++ src/pages/HomePage.styled.ts
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 
 export const HomePageContainer = styled.main`
-    height: 100vh;
+    min-height: 100vh;
     width: 100vw;
     background: rgb(24,25,26);
     background: linear-gradient(130deg, rgba(24,25,26,1) 0%, rgba(39,39,40,1) 100%);
