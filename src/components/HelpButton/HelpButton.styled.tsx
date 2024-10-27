c0e1d24b5d2a4d2a34caa30bdb834a6eb1c47d14
DJ Hemath <djhemath@gmail.com>
2024-10-27 23:26:30 +0530

    feat: helper

--- src/components/HelpButton/HelpButton.styled.tsx
+++ src/components/HelpButton/HelpButton.styled.tsx
@@ -1,6 +1,10 @@
 import styled from "styled-components";
 import { tablet } from "../../shared/Global.styled";
 
+export const HelpButtonContainer = styled.div`
+    position: relative;
+`;
+
 export const StyledHelpButton = styled.button`
     border: none;
     background-color: transparent;
@@ -28,4 +32,63 @@ export const StyledHelpButton = styled.button`
         height: 36px;
         font-size: 24px;
     `)}
+`;
+
+export const HelpContainer = styled.ul`
+    list-style-type: none;
+    background-color: #272727;
+    border: 1px solid #71717187;
+    border-radius: 8px;
+    box-shadow: 0 25px 40px 11px #00000040;
+    padding: 10px;
+
+    width: max-content;
+    display: flex;
+    flex-direction: column;
+    gap: 8px;
+
+    position: absolute;
+    right: 0px;
+    top: 60px;
+
+    transform: translateY(-5%);
+    opacity: 0;
+    z-index: -1;
+
+    transition: transform 0.2s, opacity 0.4s, z-index 0.2s;
+`;
+
+export const HelpItem = styled.li`
+    padding: 10px;
+    color: #FFF;
+    transition: background-color 0.2s;
+    border-radius: 8px;
+    cursor: pointer;
+
+    font-size: 14px;
+
+    min-height: 45px;
+    min-width: 200px;
+
+    svg {
+        width: 20px;
+        height: 20px;
+    }
+
+    &:hover {
+        background-color: #222222;
+    }
+
+    &:hover svg {
+        width: 25px;
+        height: 25px;
+    }
+`;
+
+export const HelpAnchor = styled.a`
+    display: flex;
+    gap: 12px;
+    align-items: center;
+    text-decoration: none;
+    color: inherit;
 `;
--- src/components/HelpButton/HelpButton.tsx
+++ src/components/HelpButton/HelpButton.tsx
@@ -1,7 +1,96 @@
-import { StyledHelpButton } from "./HelpButton.styled";
+import { useEffect, useRef, useState } from "react";
+import { Bitcoin, HeartIcon2, Mail, QuestionMark } from "../Icon/Icon";
+import { HelpAnchor, HelpButtonContainer, HelpContainer, HelpItem, StyledHelpButton } from "./HelpButton.styled";
+
+const HELP_ITEMS = [
+    {
+        text: "What's this?",
+        icon: <QuestionMark />,
+        link: '/snap-nostr/about',
+    },
+    {
+        text: "What's Nostr?",
+        icon: <QuestionMark />,
+        link: '/snap-nostr/about-nostr',
+    },
+    {
+        text: "Not working?",
+        icon: <QuestionMark />,
+        link: '/snap-nostr/not-working',
+    },
+    {
+        text: "Credits",
+        icon: <HeartIcon2 />,
+        link: '/snap-nostr/credits',
+    },
+    {
+        text: "Feedback",
+        icon: <Mail />,
+        link: '/snap-nostr/feedback',
+    },
+    {
+        text: "Donate",
+        icon: <Bitcoin />,
+        link: '/snap-nostr/donate',
+    },
+];
 
 export function HelpButton() {
+    const [ isOpen, setIsOpen ] = useState(false);
+    const helpContainerRef = useRef<HTMLUListElement>(null);
+    const buttonRef = useRef<HTMLButtonElement>(null);
+
+    useEffect(() => {
+        function handleClick(e: MouseEvent) {
+            if(helpContainerRef.current && !helpContainerRef.current.contains(e.target as Node)) {
+                if(buttonRef.current && !buttonRef.current.contains(e.target as Node)) {
+                    setIsOpen(false);
+                }
+            }
+        }
+
+        document.addEventListener('click', (e) => handleClick(e));
+
+        return () => {
+            document.removeEventListener('click', handleClick);
+        }
+    }, []);
+
+    const onHelpItemClick = () => {
+        setIsOpen(false);
+    }
+
     return (
-        <StyledHelpButton className="rammetto-one-regular">?</StyledHelpButton>
+        <HelpButtonContainer>
+            <StyledHelpButton
+                ref={buttonRef}
+                onClick={() => setIsOpen(prev => !prev)}
+                className="rammetto-one-regular">
+                ?
+            </StyledHelpButton>
+
+            <HelpContainer
+                ref={helpContainerRef}
+                style={{
+                    transform: isOpen ? 'translateY(0)': '',
+                    opacity: isOpen ? 1: 0,
+                    zIndex: isOpen ? 1: -1,
+                }}
+            >
+                {
+                    HELP_ITEMS.map(item => (
+                        <HelpItem onClick={() => onHelpItemClick()}>
+                            <HelpAnchor
+                                href={item.link}
+                            >
+                                {item.icon}
+                                <span>{item.text}</span>
+                            </HelpAnchor>
+                        </HelpItem>
+                    ))
+                }
+            </HelpContainer>
+            
+        </HelpButtonContainer>
     );
 }
